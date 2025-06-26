fb51aa7a91849d4a757ff00fb9c7e5f5720ef36a
GalacticLemonade <aidenmoore1820@gmail.com>
2025-06-25 21:58:47 -0600

    fix santitize

--- premake5.lua
+++ premake5.lua
@@ -105,7 +105,8 @@ project "GLFW"
 	filter { "system:windows", "configurations:Debug-AS" }	
 		runtime "Debug"
 		symbols "on"
-		sanitize { "Address" }
+		buildoptions { "-fsanitize=address" }
+		linkoptions { "-fsanitize=address" }
 		flags { "NoRuntimeChecks", "NoIncrementalLink" }
 
 	filter "configurations:Release"
